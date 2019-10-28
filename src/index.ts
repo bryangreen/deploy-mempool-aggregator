8c10e97836297c07acc4b3206923006940453a80
Bryan Green <bryogreen@gmail.com>
2019-10-28 03:51:40 -0600

    Removing expose port 10902 from docker-compose.yml as now using docker.host.internal

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import AggregatorNode from './AggregatorNode';
 
 const aggregatorNode = new AggregatorNode();
-aggregatorNode.listenIncomingTxs('http://host.docker.internal:10902/');
+aggregatorNode.listenIncomingTxs();
 aggregatorNode.emit();
 aggregatorNode.broadcastTxStream();
